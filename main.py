b4a0412978c4cabe832892faf7d8b591ef54d65f
julianlaxamana <julianlaxamana123@gmail.com>
2025-04-08 08:23:21 -0700

    added count

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ model.classes = [2]
 cap = cv2.VideoCapture(0)
 
 if __name__ == "__main__":
+    count = 0
+    prevInside = False;
     while True:
         # Read a frame from the camera
         ret, frame = cap.read()
@@ -36,14 +38,29 @@ if __name__ == "__main__":
             for index, row in df.iterrows():
                 box.append(row[col])
 
-            print(box)
             if x1 < box[0] and y1 < box[1] and x2 > box[2] and y2 > box[3]:
                 inside = True 
+                if inside and not prevInside:
+                    count += 1
+                prevInside = True
 
         if inside:
             cv2.rectangle(img, (x1, y1), (x2, y2), color=(0,255,0), thickness=2)
         else:
             cv2.rectangle(img, (x1, y1), (x2, y2), color=(0,0,255), thickness=2)
+            prevInside = False
+
+        # Text properties
+        text = "Count: " + str(count)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        position = (10, 450) # Bottom-left corner of the text
+        font_scale = 1
+        color = (255, 255, 255) # White color
+        thickness = 2
+        line_type = cv2.LINE_AA
+
+        # Write the text on the image
+        cv2.putText(img, text, position, font, font_scale, color, thickness, line_type)
 
         # Display the resulting frame
         cv2.imshow('frame', img)
